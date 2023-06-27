38b4f50a77ecaed675483b9a50698e2ddf745e23
mbackas <marc.backas@gmail.com>
2023-06-27 09:59:55 -0500

    for me

---
+++ build/CMakeFiles/sixaxispairing.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sixaxispairing.dir/main.c.o"
+  "bin/sixaxispairing"
+  "bin/sixaxispairing.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/sixaxispairing.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
